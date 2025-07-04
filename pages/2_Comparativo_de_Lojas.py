9e55d43365ad1ab646fd9b78938a13131b5b2601
Breno Duarte <breno.duarte@vbr.lan>
2025-07-04 16:57:05 -0300

    SSS dos pisos consertados

---
+++ pages/2_Comparativo_de_Lojas.py
@@ -0,0 +1,73 @@
+import ProcessamentoDaTabela as ProcTab
+import pandas as pd
+import plotly.express as px
+import streamlit as st
+from datetime import date
+
+
+# Configurações de página
+st.set_page_config(layout="wide",
+page_title= 'Relatório Nava',
+initial_sidebar_state="collapsed")
+
+
+
+# -------------------------------SIDE BAR-------------------------------- #
+
+
+## SIDE BAR ##
+st.sidebar.image(r'Imagens/NAVA-preta.png')
+
+st.sidebar.header('Filtros')
+emp = st.sidebar.radio(    "Selecione o Empreendimento",
+    options=['Viashopping', 'Viabrasil'], index = 0)
+
+ResumoLojas, DF_Fluxo, DFLojas = ProcTab.TabelaOriginal(emp)
+
+hoje = date.today()
+hoje = hoje.replace(day=1)
+
+sliderIntervalo = st.sidebar.date_input("Período",
+                     value = (date(2025,1,1),DFLojas['Data'].max()),
+                     min_value=date(2018,1,1),
+                     max_value=DFLojas['Data'].max(),
+                     format= "DD/MM/YYYY"
+)
+inicio, fim = sliderIntervalo
+inicio = inicio.replace(day=1)
+fim = fim.replace(day=1)
+inicio = pd.to_datetime(inicio)
+fim = pd.to_datetime(fim)
+segmentosUnicos = DFLojas['Segmento'].unique().tolist()
+segmentoInutil = ['Comodato', 'Depósito']
+default = [x for x in segmentosUnicos if x not in segmentoInutil]
+
+with st.sidebar.expander("Filtros Avançados", expanded=False):
+
+    SegmentosSelecionados = st.pills(
+            'Selecione os segmentos que deseja visualizar',
+        options=segmentosUnicos,
+        selection_mode= 'multi',
+        default= default
+    )
+
+    LadoSelecionado = st.segmented_control(
+        'Lado:',
+        options = ['Lado A', 'Lado B', 'Ambos'],
+        default = 'Ambos'
+    )
+
+    PisosSelecionados = st.pills(
+        'Selecione os pisos que deseja visualizar',
+        options = DFLojas['Piso'].dropna().unique().tolist(),
+        selection_mode = 'multi',
+        default = DFLojas['Piso'].dropna().unique().tolist()
+    )
+
+
+# -------------------------------FIM SIDE BAR-------------------------------- #
+
+
+# ------------------------------- COMEÇO DO COMPARATIVO -------------------------------- #
+st.title("Comparativo de Lojas")
+
